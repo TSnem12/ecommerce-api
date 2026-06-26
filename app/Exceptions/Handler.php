47cd9323016e46c3f989c815a171c97764782f07
TSnem12 <tasneemm967@gmail.com>
2026-06-26 16:17:15 +0300

    add generic exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,11 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use App\Helpers\ApiResponse;
+
 
 class Handler extends ExceptionHandler
 {
@@ -37,5 +40,21 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+
+    public function render($request, Throwable $e)
+    {
+
+        if ($e instanceof \Illuminate\Validation\ValidationException) {
+            return ApiResponse::error($e->errors(), 422);
+        }
+
+        if ($e instanceof \Illuminate\Auth\AuthenticationException) {
+            return ApiResponse::error('UnAuthenticated', 401);
+        }
+
+
+        return ApiResponse::error($e->getMessage(), 500);
     }
 }
