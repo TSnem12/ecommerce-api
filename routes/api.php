9de63c58d55550726fffa65b2de55bbae6289d26
TSnem12 <tasneemm967@gmail.com>
2026-06-27 17:24:45 +0300

    protect all routes with auth middleware

--- routes/api.php
+++ routes/api.php
@@ -25,20 +25,23 @@ Route::middleware(['auth:api'])->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
 });
 
-Route::post('/categories', [CategoryController::class, 'store']);
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/categories/{id}', [CategoryController::class, 'show']);
-Route::put('/categories/{id}', [CategoryController::class, 'update']);
-Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
 
-Route::post('/subcategories', [SubCategoryController::class, 'store']);
-Route::get('/subcategories', [SubCategoryController::class, 'index']);
-Route::get('/subcategories/{id}', [SubCategoryController::class, 'show']);
-Route::put('/subcategories/{id}', [SubCategoryController::class, 'update']);
-Route::delete('/subcategories/{id}', [SubCategoryController::class, 'destroy']);
+Route::middleware(['auth:api'])->group(function () {
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::get('/categories/{id}', [CategoryController::class, 'show']);
+    Route::put('/categories/{id}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
 
-Route::post('/products', [ProductController::class, 'store']);
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/{id}', [ProductController::class, 'show']);
-Route::put('/products/{id}', [ProductController::class, 'update']);
-Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::post('/subcategories', [SubCategoryController::class, 'store']);
+    Route::get('/subcategories', [SubCategoryController::class, 'index']);
+    Route::get('/subcategories/{id}', [SubCategoryController::class, 'show']);
+    Route::put('/subcategories/{id}', [SubCategoryController::class, 'update']);
+    Route::delete('/subcategories/{id}', [SubCategoryController::class, 'destroy']);
+
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/products/{id}', [ProductController::class, 'show']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+});
